5de37070bd78fd26a14e75d9830eee5ffaae50ac
Shahid Ilyas <159184956+Shahidgovai@users.noreply.github.com>
2024-02-25 02:57:00 +0500

    Array

---
+++ Array/main.js
@@ -0,0 +1,18 @@
+"use strict";
+//array
+let fruits = ['banana', 'orange', 'apple'];
+console.log(fruits);
+// // console.log(fruits.pop())
+// // console.log(fruits)
+// console.log(fruits.push('kiwi'))
+// // console.log(fruits)
+// console.log(fruits.shift())
+// // console.log(fruits)
+// console.log(fruits.unshift())
+// console.log(fruits)
+// console.log(fruits.unshift('pineapple'))
+// console.log(fruits)
+// console.log(fruits.splice(1,0,'kiwi','lemon'))
+// console.log(fruits)
+let newwar = fruits.slice(1);
+console.log(newwar);
--- Array/main.ts
+++ Array/main.ts
@@ -1,1 +1,18 @@
+//array
 
+let fruits:string[]=['banana','orange','apple']
+console.log(fruits)
+// // console.log(fruits.pop())
+// // console.log(fruits)
+// console.log(fruits.push('kiwi'))
+// // console.log(fruits)
+// console.log(fruits.shift())
+// // console.log(fruits)
+// console.log(fruits.unshift())
+// console.log(fruits)
+// console.log(fruits.unshift('pineapple'))
+// console.log(fruits)
+// console.log(fruits.splice(1,0,'kiwi','lemon'))
+// console.log(fruits)
+let newwar= fruits.slice(1)
+console.log(newwar)
