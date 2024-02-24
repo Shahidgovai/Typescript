e8011b06a4dd5cc32b331b4d60eebf5cccb10746
Shahid Ilyas <159184956+Shahidgovai@users.noreply.github.com>
2024-02-25 00:29:01 +0500

    Templates & Literals

---
+++ Templates & Literals/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// let myName = "shahid" ;
+// let hello = "Hello"+ myName ;
+// console.log(hello);
+// let myName = "shahid" ;
+// let hello = "Hello" + myName ;
+// console.log(hello);
+// // let hi = "Shahid";
+// // console.log(`Hello ${hi}`)
+let hi = "Shahid";
+console.log(`Hello ${hi}`);
+let firstName = "Shahid";
+let lastName = "Ilyas";
+let finalName = "Qureshi";
+let akhriName = "Qazi";
+console.log(`My Name Is ${firstName} ${lastName} ${finalName} ${akhriName}`);
--- Templates & Literals/main.ts
+++ Templates & Literals/main.ts
@@ -1,1 +1,19 @@
+// let myName = "shahid" ;
+// let hello = "Hello"+ myName ;
+// console.log(hello);
 
+// let myName = "shahid" ;
+// let hello = "Hello" + myName ;
+// console.log(hello);
+
+// // let hi = "Shahid";
+// // console.log(`Hello ${hi}`)
+
+let hi = "Shahid";
+console.log(`Hello ${hi}`)
+
+let firstName = "Shahid"
+let lastName = "Ilyas"
+let finalName = "Qureshi"
+let akhriName = "Qazi"
+console.log(`My Name Is ${firstName} ${lastName} ${finalName} ${akhriName}`);
